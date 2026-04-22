9a927252678cbf999c5e8bd214411d99e78a224e
samsj01 <samuel.suarez699@pascualbravo.edu.co>
2026-04-21 20:24:11 -0500

    Primeras Lineas de código

--- Program.cs
+++ Program.cs
@@ -10,6 +10,52 @@ namespace MiniMarket
     {
         static void Main(string[] args)
         {
+            string[] contraseñas = RegistroUser();
+        }
+
+        static string[] RegistroUser()
+        {
+            string[] usuarios = { "Administrador", "Proveedor", "Caja" };
+            string[] contraseña = new string[usuarios.Length];
+            bool userNoRegis = true;
+            int contador = 1;
+            
+            while (userNoRegis)
+            {
+                for (int i = 0; i < usuarios.Length; i++)
+                {
+                    string ingresoContraseña;
+                    string validarContraseña;
+                    do
+                    {
+                        Console.WriteLine("=== REGISTRO DE USUARIOS ===");
+                        Console.Write("Cree la contraseña del usuario " + usuarios[i] + ": ");
+                        ingresoContraseña = Console.ReadLine();
+                        Console.Write("Ingrese la contraseña nuevamente: ");
+                        validarContraseña = Console.ReadLine();
+                        if (ingresoContraseña != validarContraseña)
+                        {
+                            Console.Write("No hay similitud en el ingreso de contraseñas.\n...");
+                            Console.ReadKey();
+                            Console.Clear();
+
+                        }
+                        else
+                        {
+                            Console.Write("Contraseña Guardada.\n...");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                    } while (ingresoContraseña != validarContraseña);
+                    contador++;
+                    if (contador == usuarios.Length)
+                    {
+                        userNoRegis = false;
+                    }
+                }
+                
+            }
+            return contraseña;
         }
     }
 }
